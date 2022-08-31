d26845100f3f90109a219ef7c9c826d1cc26bc74
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-08-30 22:00:00 -0400

    start nn module

--- demo/main.py
+++ demo/main.py
@@ -1,29 +1,12 @@
-from pygrad import Scalar
+from pygrad.engine import Scalar
+from pygrad.nn import MLP
 
 
 def main():
-    # Inputs
-    x1 = Scalar(2.0)
-    x2 = Scalar(0.0)
-
-    # Weights
-    w1 = Scalar(-3.0)
-    w2 = Scalar(1.0)
-
-    # Bias
-    b = Scalar(6.8813735870195432)
-
-    x1w1 = x1*w1
-    x2w2 = x2*w2
-
-    x1w1x2xw2 = x1w1 + x2w2
-
-    n = x1w1x2xw2 + b
-
-    e = (2*n).exp()
-    o = (e - 1) / (e + 1)
-
-    o.backward()
+    x = [2.0, 3.0, -1.0]
+    model = MLP(3, [4, 4, 1])
+    out = model(x)
+    print(out)
 
 
 if __name__ == "__main__":
